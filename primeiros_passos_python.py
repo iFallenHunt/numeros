1939d41040a99b8fc08a7a95ab73640e72e9a779
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-15 17:05:25 -0300

    corrigido

--- primeiros_passos_python.py
+++ primeiros_passos_python.py
@@ -1,53 +1,12 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "primeiros passos python",
-      "provenance": [],
-      "authorship_tag": "ABX9TyODxSMMCPq9jUYs+TlaNQOp",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/iFallenHunt/numeros/blob/main/primeiros_passos_python.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "n1 = int(input('Digite o primeiro numero: '))\n",
-        "n2 = int(input('Digite o segundo numero: '))\n",
-        "\n",
-        "adicao = n1 + n2\n",
-        "subtracao = n1 - n2\n",
-        "multiplicacao = n1 * n2\n",
-        "divisao = n1 / n2 \n",
-        "\n",
-        "print('Adição ', adicao)\n",
-        "print('Subtração ', subtracao)\n",
-        "print('Multiplicação ', multiplicacao)\n",
-        "print('Divisão ', divisao)"
-      ],
-      "metadata": {
-        "id": "2bCcMM2J3X-d"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+
+adicao = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2 
+
+print('Adição ', adicao)
+print('Subtração ', subtracao)
+print('Multiplicação ', multiplicacao)
+print('Divisão ', divisao)
